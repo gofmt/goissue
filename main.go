fcb2c97a3dd28dbc2ccc52e6e9bf23323cf50deb
果子 <zmguozi@gmail.com>
2019-01-24 18:34:23 +0800

    HTTP 路由选型

--- main.go
+++ main.go
@@ -2,14 +2,17 @@ package main
 
 import (
 	"context"
+	"net/http"
 	"os"
 	"os/signal"
 	"syscall"
 
+	"goissue/api"
 	"goissue/config"
 	"goissue/models"
 	"goissue/pkgs/logger"
 
+	"github.com/go-chi/chi"
 	"github.com/sirupsen/logrus"
 )
 
@@ -42,11 +45,21 @@ func main() {
 		logrus.WithError(err).Panicln("连接数据库错误")
 	}
 
+	srv := http.Server{Addr: config.C.Addr, Handler: chi.ServerBaseContext(ctx, api.Router())}
+
+	go func() {
+		logrus.Println("API 服务监听地址: ", config.C.Addr)
+		if err := srv.ListenAndServe(); err != nil {
+			logrus.WithError(err).Panicln("API 服务监听错误")
+		}
+	}()
+
 	quit := make(chan os.Signal, 1)
 	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM, syscall.SIGKILL, syscall.SIGHUP)
 	c := <-quit
 	logrus.Debugln("程序信号: ", c)
 
+	_ = srv.Shutdown(ctx)
 	cancel()
 
 	logrus.Println("程序正常退出.")
